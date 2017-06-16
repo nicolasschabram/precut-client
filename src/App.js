b497beed716e7c7596e6ed37ef10accb94a74ddf
Nicolas Schabram <mail@nicolasschabram.de>
2017-06-16 16:03:57 +0200

    wrap app with browser router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "normalize.css";
 import "styles.css";
+
+import store from "./store";
 
 import Menu from "components/Menu";
 import Projects from "scenes/Projects";
@@ -10,16 +13,18 @@ import Projects from "scenes/Projects";
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="app">
-          <Menu />
-          <main>
-            <Switch>
-              <Route exact path="/projects" component={Projects} />
-            </Switch>
-          </main>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="app">
+            <Menu />
+            <main>
+              <Switch>
+                <Route exact path="/projects" component={Projects} />
+              </Switch>
+            </main>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
