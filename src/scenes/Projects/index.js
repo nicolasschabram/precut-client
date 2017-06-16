53cf13ecea4613fcdabd6703adcef2a13f6c9b8e
Nicolas Schabram <mail@nicolasschabram.de>
2017-06-16 16:18:01 +0200

    update refactor projects table

--- src/scenes/Projects/index.js
+++ src/scenes/Projects/index.js
@@ -1,10 +1,28 @@
 import React from "react";
+import {connect} from "react-redux";
+
 import Fold from "components/Fold";
 import TableView from "components/TableView";
 
-export default class extends React.PureComponent {
+import { getProjectCount } from "data/projects/reducer"
+
+const Projects = class extends React.PureComponent {
   render() {
-    const content = () => (<TableView location={this.props.location} />);
+    const content = () => (
+      <TableView location={this.props.location}
+                 items={this.props.projects}
+                 itemCount={this.props.projectCount}
+      />
+    );
     return <Fold content={content} />;
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    projects: state.get("projects"),
+    itemCount: getProjectCount(state.get("projects"))
+  };
+}
+
+export default connect(mapStateToProps)(Projects);
