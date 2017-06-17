b7e272b0eabe32234d1705ab1a2eded52c18e13e
Nicolas Schabram <mail@nicolasschabram.de>
2017-06-17 19:39:12 +0200

    minor reorganization

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
 
 import "normalize.css";
 import "styles.css";
 
-import store from "./store";
-
 import Menu from "components/Menu";
 import Projects from "scenes/Projects";
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="app">
-            <Menu />
-            <main>
-              <Switch>
-                <Route exact path="/projects" component={Projects} />
-              </Switch>
-            </main>
-          </div>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <div className="app">
+          <Menu />
+          <main>
+            <Switch>
+              <Route exact
+                     path="/projects"
+                     component={Projects}
+                     onLeave={() => this.props.resetSelection}
+              />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
     );
   }
 }
-
-
-
-export default App;
